26ff137004bd6ff396564d6761b817f15c209a8e
manakouri <rachel@russellst.school.nz>
2025-10-05 18:40:22 +1300

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,24 @@
+// firebase-config.js
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "httpshttps://www.gstatic.com/firebasejs/9.10.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-firestore.js";
+
+// TODO: Replace with your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAakXPbECFWlXMjG-EpB5_NSPcDWK0BkjY",
+  authDomain: "place-value-shifter.firebaseapp.com",
+  projectId: "place-value-shifter",
+  storageBucket: "place-value-shifter.firebasestorage.app",
+  messagingSenderId: "388817467603",
+  appId: "1:388817467603:web:26f323802399f959dd7c26",
+  measurementId: "G-TMZC74RWR2"
+};
+
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+// Export for use in other scripts
+export { db };
